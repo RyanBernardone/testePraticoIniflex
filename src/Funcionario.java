6d0fe49dc41138e6ee86a0c6852a1f77e983a1db
Ryan <ryan.bernardone@gmail.com>
2026-04-09 20:02:05 -0300

    feat: Criado estrutura do projeto
    ## Description
    
    Criado a estrutura de classes principal do projeto e preenchido com os seus atributos, construtores, getters e setter.

---
+++ src/Funcionario.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+    private BigDecimal salary;
+    private String function;
+
+    public Funcionario(String name, LocalDate nascimento, BigDecimal salary, String function) {
+        super(name, nascimento);
+        this.salary = salary;
+        this.function = function;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String name;
+    private LocalDate nascimento;
+
+    public Pessoa(String name, LocalDate nascimento) {
+        this.name = name;
+        this.nascimento = nascimento;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+}
